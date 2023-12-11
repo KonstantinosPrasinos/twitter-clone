922ce1cfe3a4a19043ee7ce1330892cead7c2b0a
Konstantinos Prasinos <konstantinos.prasinos@gmail.com>
2023-12-11 14:46:06 +0200

    feat: add user context feat: add router feat: add protected routes feat: add Home route feat: add Register route feat: add theme colors to css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {createBrowserRouter, Navigate, Outlet, RouterProvider} from "react-router-dom";
+import userContext from "./context/UserContext.jsx";
+import Home from "./pages/Home.jsx";
+import Register from "./pages/Register.jsx";
+
+function ProtectedLayout() {
+  // If the user is not connected, navigate to log in
+  if (!userContext.state?.id)
+    return <Navigate to={"/login"} replace />
+
+  // When we want to have certain elements that always stay visible, like a navigation bar, we add it below
+  return (
+      <Outlet />
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <ProtectedLayout />,
+    children: [
+        // Add here all protected routes
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/home',
+        element: <Navigate to={"/"} />
+      }
+    ]
+  },
+    // All non protected routes go here
+  {
+    path: '/register',
+    element: <Register />
+  }
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            This is where you register to the app
+        </div>
+    );
+};
+
+export default Register;
