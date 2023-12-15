42f9f817bb37217d11e9fb43e013843ff8d38146
Melina Moraiti <melinamoraith@gmail.com>
2023-12-15 20:13:28 +0200

    set up prisma configuration

--- server/server.js
+++ server/server.js
@@ -10,10 +10,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get("/api", (req, res) => {
+    res.json({email: "user1@mail.com",password:"pass123" });
+});
+app.get("", (req, res) => {
+    res.send("Hello world!");
 });
 
-app.use('/auth', authRoutes);
+app.use('/api', authRoutes);
 
 app.listen(PORT,() => console.log(`Server started at: http://localhost:${PORT}`));
