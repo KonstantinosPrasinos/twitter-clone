5bce08e6f92bfbe406904176351f13defd619929
Melina Moraiti <melinamoraith@gmail.com>
2023-12-15 23:08:02 +0200

    add database url to configuration

--- server/config/app-config.js
+++ server/config/app-config.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const config = {
   supabaseUrl: process.env.SUPABASE_URL,
   supabaseApiKey: process.env.SUPABASE_API_KEY,
+  connectionString: process.env.DATABASE_URL,
   port: process.env.PORT,
 };
 
-module.exports = config;
+export default config;
