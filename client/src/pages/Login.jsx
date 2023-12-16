bfc186a2ecb28b2ae3263af050cba85a01dccb7a
Andrewmgk <151464838+Andrewmgk@users.noreply.github.com>
2023-12-16 20:54:55 +0200

    Changed login code

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,43 +1,49 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 const Login = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showWarning,SetsShowWarning]=useState(false);
     const navigate = useNavigate();
+    
+   
 
     const handleLogin = () => {
 
+        e.preventDefault();
+        
+        if(username.length === 0 ) {
+            return;
+        }
+
+        if(password.length === 0) {
+
+            return;
+        }
+
     };
 
+
+    const handleClick = () => {
+        navigate("/Register");
+    };
+
+      
+
     return (
-        <div className={"Panel Vertical-FLex-Container"}>
-            {
-            (
-                <div>
-                    <h1>Login to TSIOY</h1>
-                    <label>
-                        Username or Email:
-                        <input
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </label>
-                    <br />
-                    <label>
-                        Password:
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </label>
-                    <br />
-                    <button onClick={handleLogin}>Login</button>
-                </div>
-            )}
-        </div>
+        <div className={"Panel Vertical-Flex-Container"}>
+            <h2>Login to TSIOY</h2>
+            <from  className={"Vertical-Flex-Container"} onSubmt={handleLogin}>
+                <label for="Username">Username or Email</label>
+                <input value={username} onChange={(e) => setUsername(e.target.value)}type="username" id="username" name="username" />
+                <label for="Password">Password</label>
+                <input value={password} onChange={(e) => setPassword(e.target.value)}type="password" id="password" name="password" />
+                <button type="Login">Log In</button>  
+            </from>
+            <button onClick={handleClick}>Don't have an account register here.</button>
+        </div>    
     );
 }
 
