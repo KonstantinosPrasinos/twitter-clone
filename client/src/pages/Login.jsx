612740f79b03510fcff4a0c26c38ad0d68bec496
Melina Moraiti <melinamoraith@gmail.com>
2023-12-16 19:26:26 +0200

    handle login implemented for testing
     handle login implemented using the login post route for testing

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 const Login = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-
-    const handleLogin = () => {
-
+    const handleLogin = async () => {
+        try{
+            const response = await fetch("http://localhost:3000/api/login", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            });
+            if (response.ok) {
+                const responseData = await response.json();
+                localStorage.setItem('jwt', responseData.token);
+                navigate('/register');
+            } else {
+                const errorData = await response.json();
+                console.error("Login failed: ", errorData.message);
+            }
+        }
+        catch(error)
+        {
+            console.error("Error during login: ",error);
+        }
     };
-
     return (
         <div className={"Panel Vertical-FLex-Container"}>
             {
