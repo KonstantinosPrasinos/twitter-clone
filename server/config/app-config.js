37b0ff832ad710729fd02edcf84a3fd8154728bf
marioskam3 <85133854+marioskam3@users.noreply.github.com>
2023-12-17 01:12:05 +0200

    feat(backend): added Prisma - Create user
    example user created successfully - postman tested

--- server/config/app-config.js
+++ server/config/app-config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const config = {
   supabaseUrl: process.env.SUPABASE_URL,
   supabaseApiKey: process.env.SUPABASE_API_KEY,
+  connectionString: process.env.DATABASE_URL,
   port: process.env.PORT,
 };
 
--- server/controllers/signupController.js
+++ server/controllers/signupController.js
@@ -1,4 +1,6 @@
-const db = require('../database/dbconnect');
+//const db = require('../database/dbconnect');
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 const signupController = async (req, res) => {
     const { username, email, password, repeatPassword } = req.body;
@@ -7,15 +9,18 @@ const signupController = async (req, res) => {
     if (password !== repeatPassword) {
         return res.status(400).json({ success: false, message: 'Passwords do not match' });
     }
-
+    
     try {
-      const { user, error } = await db.auth.signUp(
-        {
-            username,
-            email,
-            password
-        }
-      );
+      const { user, error } = await prisma.users.create({
+        data: {
+          username,
+          email,
+          password_hash: password,
+          isadmin: false,
+          created_at: new Date()
+
+        },
+      });
       
       console.log(`Username: ${username}, Email: ${email}, Password: ${password}`);
 
