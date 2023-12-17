2e3c6ed0c1a7868b08533f73c6a75d7c7ea830e8
marioskam3 <85133854+marioskam3@users.noreply.github.com>
2023-12-17 18:17:58 +0200

    feat(backend): Error handling

--- server/controllers/signupController.js
+++ server/controllers/signupController.js
@@ -9,9 +9,11 @@ const signupController = async (req, res) => {
     if (password !== repeatPassword) {
         return res.status(400).json({ success: false, message: 'Passwords do not match' });
     }
+
+    
     
     try {
-      const { user, error } = await prisma.users.create({
+      const user = await prisma.users.create({
         data: {
           username,
           email,
@@ -23,17 +25,16 @@ const signupController = async (req, res) => {
       });
       
       console.log(`Username: ${username}, Email: ${email}, Password: ${password}`);
-
-      if (error) {
-        return res.status(500).json({ success: false, message: 'Failed to sign up', error });
-      } 
-      else {
-        return res.json({ success: true, message: 'Sign up successful', user });
-      }
+      return res.json({ success: true, message: 'Sign up successful', user });
 
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, message: 'Internal server error' });
+      if (error.code === "P2002") {
+        res.status(400).json({ success: false, message: 'Username or email already exists' });
+      }
+      else{
+        console.error('Error during signup:',error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+      }
     }
   };
 
