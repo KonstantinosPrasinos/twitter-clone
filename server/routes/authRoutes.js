6f6ad55fb1723e4264c4ead3cc785681a91630f1
Melina Moraiti <melinamoraith@gmail.com>
2023-12-22 21:25:39 +0200

    Require middleware to protect the routes.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const authenticateToken = require('./middleware/authenticateToken');
 const {login_post}  = require('../controllers/authController');
 
 router.post('/api/login', login_post);
