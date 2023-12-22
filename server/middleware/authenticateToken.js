6bc77b6c6571d0df78caaa38e52be569ab35c167
Melina Moraiti <melinamoraith@gmail.com>
2023-12-22 21:18:03 +0200

    Create a middleware function for authentication

---
+++ server/middleware/authenticateToken.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const appConfig = require('../config/app-config');
+
+function authenticateToken(req, res, next) {
+  const token = req.cookies.jwt; 
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
+  }
+
+  jwt.verify(token, appConfig.secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Forbidden: Invalid token' });
+    }
+
+    req.user = decoded; 
+    next();
+  });
+}
+
+module.exports = authenticateToken;
