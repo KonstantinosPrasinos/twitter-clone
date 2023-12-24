c8647f6bc48a45e846324cf914cf572668fd9c79
Konstantinos Prasinos <74455528+KonstantinosPrasinos@users.noreply.github.com>
2023-12-24 11:22:44 +0200

    fix(frontend): home page doesn't work

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-
-    const { loggedIn, email } = props
-    const navigate = useNavigate();
-
     return <div className="mainContainer">
         <div className={"titleContainer"}>
             <div>Welcome!</div>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,50 +3,38 @@ import { useNavigate } from "react-router-dom";
 import {LoginApp} from "../functions/LoginApp.jsx";
 
 
-const Login = (props) => {
+const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-    const  login = LoginApp();
-    
-   
+    const login = LoginApp();
 
     const handleLogin = async (event) => {
-
       event.preventDefault();
 
       const response = await login(username, password);
-
-        if (response === true) {
-            navigate("/home");
-        } else {
-            console.log("Failed to Login")
-        }
-    
-
-        
-
+      if (response) {
+          navigate("/home");
+      } else {
+          console.log("Failed to Login")
+      }
     };
-
 
     const handleClick = () => {
         navigate("/Register");
     };
-
-      
 
     return (
         <div className={"Panel Vertical-Flex-Container"}>
             <h2>Login to TSIOY</h2>
             <form  className={"Vertical-Flex-Container"} onSubmit={handleLogin}>
                 <label htmlFor="Username">Username or Email</label>
-                <input value={username} onChange={(e) => setUsername(e.target.value)}type="username" id="username" name="username" required />
+                <input value={username} onChange={(e) => setUsername(e.target.value)} type="username" id="username" name="username" required />
                 <label htmlFor="Password">Password</label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)}type="password" id="password" name="password" required />
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" required />
                 <button type="Login">Log In</button>  
             </form>
             <button onClick={handleClick}>Don't have an account register here.</button>
-            
         </div>    
     );
 }
