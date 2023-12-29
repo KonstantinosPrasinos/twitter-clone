b53c5f4b717d97c0f4c03765c5c8cbdf20ab6a27
Melina Moraiti <melinamoraith@gmail.com>
2023-12-29 12:46:18 +0200

    Check if post's content is empty

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -3,20 +3,28 @@ const prisma = new PrismaClient();
 
 const createPost = async (req,res) => {
     const {userId,postContent} = req.body;
-    try{
-        const newPost = await prisma.posts.create({
-            data: {
-              user_id: userId,
-              content: postContent,
-            },
-        });
-        res.status(201).json(newPost);
-    }
-    catch (error)
+    if (!postContent)
     {
-        console.error('Error creating post:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        try{
+            const newPost = await prisma.posts.create({
+                data: {
+                  user_id: userId,
+                  content: postContent,
+                },
+            });
+            res.status(201).json(newPost);
+        }
+        catch (error)
+        {
+            console.error('Error creating post:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
     }
+    else {
+        console.error('Post Content is Empty!');
+        res.status(400).json({ error: 'Post Content is Empty!' });
+    }
+
 }
 
 module.exports = createPost;
