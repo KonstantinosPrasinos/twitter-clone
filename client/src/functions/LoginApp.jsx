479c0398a9ebf160536c7b5249bf3d138fea2227
Melina Moraiti <melinamoraith@gmail.com>
2023-12-29 12:07:16 +0200

    Add the cookie in the post request

--- client/src/functions/LoginApp.jsx
+++ client/src/functions/LoginApp.jsx
@@ -8,7 +8,8 @@ export const LoginApp = () => {
         const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/login`, {
             method: 'POST',
             body: JSON.stringify({username, password}),
-            headers: {'Content-Type': 'application/json'}
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include'
         });
 
         if (!response.ok) {
