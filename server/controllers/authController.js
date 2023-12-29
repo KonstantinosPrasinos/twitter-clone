1b6d1ab6c0ca573d2fa997d27fd6c6c60c5ace75
Melina Moraiti <melinamoraith@gmail.com>
2023-12-29 13:35:07 +0200

    Create a logout controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -76,4 +76,19 @@ async function login_post(req, res) {
     await prisma.$disconnect();
   }
 }
-module.exports = { login_post, findUserByCredential};
+const logoutController = (req, res) => {
+  try {
+    // Clear the authentication token on the server side
+    const options = {
+      expires: new Date(0),
+      httpOnly: true,
+    };
+    // Clear the authentication cookie
+    res.cookie('jwt', '', options);
+    res.status(200).json({ message: 'Logout successful' });
+  } catch (error) {
+    console.error('Error during logout:', error);
+    res.status(500).json({ error: 'Internal server error during logout' });
+  }
+};
+module.exports = { login_post, findUserByCredential,logoutController};
