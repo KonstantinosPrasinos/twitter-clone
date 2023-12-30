471e767a272bac15edd0794e7cd4de3c20a4860c
Melina Moraiti <melinamoraith@gmail.com>
2023-12-30 11:51:05 +0200

    Create a useLogout Hook

---
+++ client/src/hooks/useLogout.jsx
@@ -0,0 +1,24 @@
+import { useCallback } from 'react';
+import { useNavigate } from "react-router-dom";
+const useLogout = () => {
+  const navigate  = useNavigate();
+  const logout = useCallback(async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/logout`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include'
+      });
+      if (response.ok) {
+        navigate('/login');
+      } else {
+        const errorData = await response.json();
+        console.error('Logout failed:', errorData);
+      }
+    } catch (error) {
+      console.error('Error during logout:', error);
+    }
+  }, []);
+  return { logout };
+};
+export default useLogout;
