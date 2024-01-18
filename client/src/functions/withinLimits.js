5daf21d032de7393ca72ed8dfef2f314bdaccae3
Melina Moraiti <melinamoraith@gmail.com>
2024-01-18 17:05:42 +0200

    Fix: export withinLimits

--- client/src/functions/withinLimits.js
+++ client/src/functions/withinLimits.js
@@ -1,2 +1,2 @@
 // Utility function to check if a string length is within specified limits
-const withinLimits = (value, min, max) => value.length >= min && value.length <= max;
+export const withinLimits = (value, min, max) => value.length >= min && value.length <= max;
