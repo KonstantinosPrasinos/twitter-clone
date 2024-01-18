6403f09d5e87f6207ba8e76f748722ef22737fba
Melina Moraiti <melinamoraith@gmail.com>
2024-01-18 16:49:07 +0200

    Create SearchComponent

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState} from 'react';
+import {withinLimits} from "../functions/withinLimits.js";
+
+const Search = () => {
+    const [searchQuery,setSearchQuery] = useState("");
+    const [searchResults,setSearchResults] = useState({users: [], posts: []});
+    const [error,setError] = useState(null);
+
+    const handleInput = (e) =>
+    {
+        const inputQuery = e.target.value;
+        if (withinLimits(inputQuery,2,50))
+        {
+            setSearchQuery(inputQuery);
+            setError(null);
+        }
+        else setError('Search query must be between 2 and 50 characters.');
+
+    };
+
+    const handleSearch = async () =>{
+        try{
+            const response = await fetch('/api/search', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ searchQuery }),
+                credentials: 'include'
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            setSearchResults(data);
+            setError(null)
+        } 
+        catch (error) {
+            console.error('Error during search:', error);
+        }
+    };
+    useEffect(() => {
+        if (withinLimits(searchQuery, 2, 50)) {
+            handleSearch();
+            setError(null);
+        }
+        else setError('Search query must be between 2 and 50 characters.');
+    }, [searchQuery]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={handleInput}
+        placeholder="Search Twitter..."
+      />
+
+      <button onClick={handleSearch}>Search</button>
+
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+
+      <div>
+        <h2>Users</h2>
+        <ul>
+          {searchResults.users.map((user) => (
+            <li key={user.id}>{user.username}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h2>Posts</h2>
+        <ul>
+          {searchResults.posts.map((post) => (
+            <li key={post.id}>
+              <p>{post.content}</p>
+              <p>Author: {post.users.username}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
