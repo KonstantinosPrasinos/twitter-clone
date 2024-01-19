675210a35b466814c84aee78a6ac1db66e42e9ec
Konstantinos Prasinos <74455528+KonstantinosPrasinos@users.noreply.github.com>
2024-01-19 16:48:58 +0200

    style: remove margin from post username

--- client/src/components/ViewPostsForm.jsx
+++ client/src/components/ViewPostsForm.jsx
@@ -47,7 +47,7 @@ const ViewPostsForm = () => {
             <div key={post.isRepost ? `repost_${post.repost_id}` : `post_${post.post_id}`}>
               <div className="Single-Post-Container">
                 <div>
-                  <h2>{post.isRepost ? post.reposted_username : post.username}</h2>
+                  <h2 className={"post-username"}>{post.isRepost ? post.reposted_username : post.username}</h2>
                   <p style={{ fontSize: '16px', fontStyle: 'italic' }}>Said:</p>
                   <p>{post.content}</p>
                   {post.isRepost && <p style={{ fontSize: '16px', fontStyle: 'italic' }}>Repost by: {post.original_username}</p>}
