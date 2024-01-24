ca1b5384f9ae9e52c6fba241dcc0cfcd22443860
Melina Moraiti <melinamoraith@gmail.com>
2024-01-24 15:39:04 +0200

    Create Results.jsx

---
+++ client/src/pages/Results.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Search from '../components/Search.jsx';
+import LogoutButton from '../components/LogoutButton.jsx';
+const Results = () => {
+
+}
+export default Results
