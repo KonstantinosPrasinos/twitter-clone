55e89bf68eec78f29e9317a8e5d4af3911508191
marioskam3 <85133854+marioskam3@users.noreply.github.com>
2024-01-24 15:53:49 +0200

    feat(backend): like and unlike a post
    unlike -> take you like back
    
    req.body
    {
        "user_id"  : userid,
        "post_id" : postid
    }

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const createPost = require('../controllers/postController');
+const {createPost,likePost,unlikePost} = require('../controllers/postController');
 const authenticateToken = require('../middleware/authenticateToken'); //protects routes
 const router = express.Router();
 
 router.post('/api/posts', authenticateToken, createPost);
+router.post('/api/post/like', authenticateToken, likePost);
+router.post('/api/post/unlike', authenticateToken, unlikePost);
+
 
 module.exports = router;
