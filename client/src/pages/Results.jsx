1ecf5f8524d83bf03e211e203d4438b9e5c4e05c
Melina Moraiti <melinamoraith@gmail.com>
2024-01-24 18:28:47 +0200

    Add props to reusable component

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import Search from '../components/Search.jsx';
 import LogoutButton from '../components/LogoutButton.jsx';
 const Results = () => {
+    return (
+        <div className="mainContainer">
+            <LogoutButton />
+            <div >
+                <Search isHomePage={false} maxResults={20}/> 
+            </div>
+            
+        </div>
+    );
+};
 
-}
 export default Results
