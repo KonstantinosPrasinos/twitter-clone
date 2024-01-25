b3eb67464de6d5fa2dabe2940aac547a3c188fa1
Melina Moraiti <melinamoraith@gmail.com>
2024-01-25 11:09:36 +0200

    Fix: positions search, logout using home styles

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -4,9 +4,11 @@ import LogoutButton from '../components/LogoutButton.jsx';
 const Results = () => {
     return (
         <div className="mainContainer">
-            <LogoutButton />
-            <div >
-                <Search customStyle={{position:'relative', top:  '0',right:'20%', width:'130%'}} maxResults={20}/> 
+            <div className={"mainContainer-left-bar "}>
+                <Search  maxResults={20}/> 
+            </div>
+            <div className={"mainContainer-right-bar Vertical-Flex-Container"}>
+                <LogoutButton />
             </div>
             
         </div>
