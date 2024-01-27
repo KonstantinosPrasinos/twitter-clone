83f2a5449923bce3b67da6ad3470f525547f5c31
marioskam3 <85133854+marioskam3@users.noreply.github.com>
2024-01-27 13:25:56 +0200

    fix(backend): Auth user can view profile
    check happens on feedController

--- server/routes/feedRoutes.js
+++ server/routes/feedRoutes.js
@@ -3,13 +3,6 @@ const createFeed = require('../controllers/feedController');
 const authenticateToken = require('../middleware/authenticateToken'); //protects routes
 const router = express.Router();
 
-router.get('/api/feed/:user_id', authenticateToken, (req, res) => {
-    const authenticatedUserId = req.user.userId;
-    const requestedUserId = parseInt(req.params.user_id, 10);
-    if (authenticatedUserId !== requestedUserId) {
-      return res.status(403).json({ message: 'Forbidden: Access denied. Invalid user ID.' });
-    }
-    createFeed(req, res);
-  });
+router.get('/api/feed/:user_id', authenticateToken,createFeed);
 
 module.exports = router;
