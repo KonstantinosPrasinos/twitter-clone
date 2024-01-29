451f6b54dede8efcec2917147e2a07fdf32f4ab0
Andrewmgk <151464838+Andrewmgk@users.noreply.github.com>
2024-01-29 11:10:11 +0200

    Remove commendpost

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -9,7 +9,6 @@ router.post('/api/post/unlike', authenticateToken, unlikePost);
 router.post('/api/post/repost', authenticateToken, repostPost);
 router.post('/api/post/unrepost', authenticateToken, unrepostPost);
 router.post('/api/post/reply', authenticateToken, replyPost);
-router.post('/api/post/:post_id/replies', authenticateToken, commentPost);
 
 
 module.exports = router;
