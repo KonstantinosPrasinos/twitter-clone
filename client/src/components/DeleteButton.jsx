00e1d5b17e8a1931c92a8b05e6e13fcbb24e1009
Melina Moraiti <melinamoraith@gmail.com>
2024-02-12 11:05:14 +0200

    Logout if session expired

--- client/src/components/DeleteButton.jsx
+++ client/src/components/DeleteButton.jsx
@@ -2,19 +2,22 @@ import React, { useCallback,useContext,useState } from 'react';
 import useDeleteResource from '../hooks/useDeleteResource';
 import { FaRegTrashAlt,FaTrashAlt } from "react-icons/fa";
 import {AlertContext} from "../context/AlertContext.jsx";
+import useLogout from "../hooks/useLogout.jsx";
 const DeleteButton = ({ label, resourceType, resourceId }) => {
     const alertContext = useContext(AlertContext);
     const deleteResource = useDeleteResource();
     const [isHovered, setIsHovered] = useState(false);
     const [isClicked, setIsClicked] = useState(false);
+    const {logout} = useLogout();
     const handleDelete = useCallback(async () => {
         try {
             const response = await deleteResource(resourceType, resourceId);
             if (!response.ok) {
                 const error = await response.json();
                 alertContext.addAlert(error.message);
+                if(response.status === 401) await logout(); //Session Expired
             } else {
-                alertContext.addAlert("Post Successfuly deleted");
+                alertContext.addAlert(`${resourceType} has been successfully deleted`);
             }
         } catch (error) {
             console.error('Error deleting resource:', error);
