7f8da2b02d7a93aaa2185227314d3dcaa5007d5b
Melina Moraiti <melinamoraith@gmail.com>
2024-02-24 08:19:48 +0200

    jwt token lasts for 8 hours

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -26,7 +26,7 @@ async function findUserByCredential(credential) {
   }
   else console.error("credential given is undefined");
 }
-const maxTokenAge = 60 * 1000; //8 hours
+const maxTokenAge = 8 * 60 * 60 * 1000; //8 hours
 function createToken(user) {
   const payload = {
     userId: user.user_id,
